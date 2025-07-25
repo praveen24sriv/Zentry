a577df642875ef01db391cf865e00d94d440052b
praveen24sriv <praveen24.srivastav@gmail.com>
2025-07-25 14:23:42 +0530

    RTK

--- ZENTRY/src/App.jsx
+++ ZENTRY/src/App.jsx
@@ -1,13 +1,40 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Header, Footer } from "./components/index.js";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    //finally always runs
+  }, []);
 
-  return (
-    <>
-      <h1> Hey there</h1>
-    </>
-  );
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
