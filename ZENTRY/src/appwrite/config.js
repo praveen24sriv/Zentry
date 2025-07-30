b086f755ca10e6cf38ec1bd9680a16b82ef9242b
praveen24sriv <praveen24.srivastav@gmail.com>
2025-07-30 22:53:31 +0530

    debugging

--- ZENTRY/src/appwrite/config.js
+++ ZENTRY/src/appwrite/config.js
@@ -1,5 +1,6 @@
 import conf from "../conf/conf.js";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
+import { Permission, Role } from "appwrite";
 
 export class Service {
   client = new Client();
@@ -98,10 +99,13 @@ export class Service {
       return await this.bucket.createFile(
         conf.appwriteBucketId,
         ID.unique(),
-        file
+        file,
+        [
+          Permission.read(Role.any()), // 👈 REQUIRED for public preview
+        ]
       );
     } catch (error) {
-      console.log("Appwrite serive :: uploadFile :: error", error);
+      console.log("Appwrite service :: uploadFile :: error", error);
       return false;
     }
   }
